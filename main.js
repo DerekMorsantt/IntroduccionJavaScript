ff4b8d7fc02851bbf3c2b135ca93c56c94181bc8
Derek Morales <derekmorsan@gmail.com>
2025-07-08 08:57:55 -0600

    Introduccion a JS

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("[ Variables y tipos de datos ]");
+console.log("------------------------------");
+// Variables
+
+//var edad = 10;
+//var estado = true;
+//var nombre = "Derek";
+
+let edad = 10;
+let estado = true;
+let nombre = "Thomas";
+
+// Declarar una constante en java
+// final float PI = 3.1416;
+
+// Las constantes siempre con mayusculas
+const PI = 3.1416;
+
+console.log(edad, estado, nombre, PI);
