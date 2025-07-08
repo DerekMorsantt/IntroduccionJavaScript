4dc6416175e8d341c31a231489d6725a6cfbd06f
Derek Morales <derekmorsan@gmail.com>
2025-07-08 09:16:53 -0600

    Operadores matematicos

---
+++ operadorMatematicos.js
@@ -0,0 +1,23 @@
+console.log("[ Operadores matematicos ]");
+console.log("-----------------------------");
+
+
+let fecha = 2025;
+let edad1 = 17;
+
+// fecha de nacimiento
+
+let resultado = fecha - edad1;
+
+console.log("resultado de la resta:", resultado)
+
+let num1 = 10;
+let num2 = 20;
+
+let resultado1 = num1 + num2;
+let resultado2 = num1 / num2;
+let resultado3 = num1 * num2;
+
+console.log("resultado de la suma:", resultado1)
+console.log("resultado de la division:", resultado2)
+console.log("resultado de la multiplicacion:", resultado3)
